c10f36a6eaf108914bcc1efe6fc107a18d89e536
ZinMinHtet21801 <zetminhtin@gmail.com>
2022-09-20 00:40:24 +0630

    removed unused imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { ChakraProvider, createStandaloneToast } from "@chakra-ui/react";
-import React, { createContext, useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
 import App from "./App";
