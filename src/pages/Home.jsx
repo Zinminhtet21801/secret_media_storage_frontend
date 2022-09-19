3ce7afba0a498dcfce6c8371f0fa73fd970e590c
ZinMinHtet21801 <zetminhtin@gmail.com>
2022-09-20 00:41:56 +0630

    removed imported Compose1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,12 @@
 import { Box, HStack } from "@chakra-ui/react";
 import Compose from "../components/Compose";
-import Compose1 from "../components/Compose1";
 // import {add} from "react-icons/all"
 const Home = () => {
   return (
     <Box>
       <Box w={"80%"}>
         <HStack>
-            <Compose1 />
+            <Compose />
         </HStack>
       </Box>
     </Box>
