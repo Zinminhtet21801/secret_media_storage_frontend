06658db91a817b84710d232395235911391a5514
ZinMinHtet21801 <zetminhtin@gmail.com>
2022-11-21 20:11:38 +0630

    added react-query provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,11 @@
 import { ChakraProvider, createStandaloneToast } from "@chakra-ui/react";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
@@ -7,11 +14,17 @@ import "./index.css";
 
 const { ToastContainer, toast } = createStandaloneToast();
 
+export const baseURL = import.meta.env.VITE_BASE_URL;
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RecoilRoot>
-    <ChakraProvider>
-      <App />
-      <ToastContainer />
-    </ChakraProvider>
-  </RecoilRoot>
+  <QueryClientProvider client={queryClient}>
+    <RecoilRoot>
+      <ChakraProvider>
+        <App />
+        <ToastContainer />
+      </ChakraProvider>
+    </RecoilRoot>
+  </QueryClientProvider>
 );
