b0447a36684e5b54c70aa437b3e4f96c3cd2c570
ZinMinHtet21801 <zetminhtin@gmail.com>
2022-11-21 20:12:32 +0630

    added categories array

---
+++ src/assets/Categories.js
@@ -0,0 +1,18 @@
+export const categories = [
+  {
+    id: 1,
+    name: "Image",
+  },
+  {
+    id: 2,
+    name: "Audio",
+  },
+  {
+    id: 3,
+    name: "Video",
+  },
+  {
+    id: 4,
+    name: "Others",
+  },
+];
