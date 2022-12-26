4fa1885c24fd4690be5cebbe8331491ae836916c
ZinMinHtet21801 <zetminhtin@gmail.com>
2022-12-26 16:06:51 +0630

    removed authorization header, added withCredentials atrribute

--- src/services/getQuantityCounts.js
+++ src/services/getQuantityCounts.js
@@ -3,13 +3,13 @@ import axios from "axios";
 import { baseURL } from "../main.jsx";
 
 export const getQuantityCounts = async () => {
-  console.log('getQuantityCounts 2')
   // write some codes to fetch items count from server
   try {
     const { data } = await axios.get(baseURL + "/media/getQuantity", {
-      headers: {
-        Authorization: `Bearer ${document.cookie.split("=")[1]}`,
-      },
+      // headers: {
+      //   Authorization: `Bearer ${document.cookie.split("=")[1]}`,
+      // },
+      withCredentials: true,
     });
     return data;
   } catch (e) {
