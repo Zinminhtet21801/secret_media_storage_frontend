48e259bef723b255d909c696af62a7d57a8cc87f
ZinMinHtet21801 <zetminhtin@gmail.com>
2023-01-29 22:26:00 +0630

    refactor code

--- src/react-query/queryClient.js
+++ src/react-query/queryClient.js
@@ -8,7 +8,6 @@ function queryErrorHandler(error) {
   const id = "react-query-error";
   const title =
     error instanceof Error ? error.message : "error connecting to server";
-
   // prevent duplicate toasts
   toast.closeAll();
   toast({ id, title, status: "error", variant: "subtle", isClosable: true });
