099c4523ddaa51f077c1b434ad3b61e36b7c90d1
Zinminhtet21801 <zetminhtin@gmail.com>
2023-03-04 01:46:04 +0630

    alertDialog for removing items from db

---
+++ src/components/AlertDialog.jsx
@@ -0,0 +1,46 @@
+import { useRef } from "react";
+
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogCloseButton,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+} from "@chakra-ui/react";
+
+const ConfirmAlertDialog = ({ remove, isOpen, onClose }) => {
+  const cancelRef = useRef();
+
+  return (
+    <AlertDialog
+      motionPreset="slideInBottom"
+      leastDestructiveRef={cancelRef}
+      onClose={onClose}
+      isOpen={isOpen}
+      isCentered
+    >
+      <AlertDialogOverlay />
+
+      <AlertDialogContent>
+        <AlertDialogHeader>Discard Changes?</AlertDialogHeader>
+        <AlertDialogCloseButton />
+        <AlertDialogBody>
+          Are you sure you want to delete this item? You can't undo this action.
+        </AlertDialogBody>
+        <AlertDialogFooter>
+          <Button ref={cancelRef} onClick={onClose}>
+            No
+          </Button>
+          <Button colorScheme="red" ml={3} onClick={remove}>
+            Yes
+          </Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default ConfirmAlertDialog;
