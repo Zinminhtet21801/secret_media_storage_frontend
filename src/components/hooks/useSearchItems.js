b5eafaa8443aca3a4382c397f5b6afe196c84d08
Zinminhtet21801 <zetminhtin@gmail.com>
2023-03-05 22:35:57 +0630

    refactor and make returned data global state

--- src/components/hooks/useSearchItems.js
+++ src/components/hooks/useSearchItems.js
@@ -1,93 +1,128 @@
-import { useState, useEffect } from "react";
+import { useEffect, useCallback } from "react";
 import { AxiosInstance } from "../../axios/axiosInstance";
+import { useRecoilState } from "recoil";
+import { searchState } from "../../atoms/atoms";
 
 const useSearchItems = () => {
-  const [searchItems, setSearchItems] = useState([]);
-  const [term, setTerm] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [submitClicked, setSubmitClicked] = useState(false);
-  const [total, setTotal] = useState(0);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [search, setSearch] = useRecoilState(searchState);
 
   const handleChange = (event) => {
-    setTerm(event.target.value);
+    setSearch((prev) => ({
+      ...prev,
+      term: event.target.value,
+    }));
+  };
+
+  const setTerm = (text) => {
+    setSearch((prev) => ({
+      ...prev,
+      term: text,
+    }));
+  };
+
+  const handleSubmitClicked = (bool) => {
+    setSearch((prev) => ({
+      ...prev,
+      submitClicked: bool,
+    }));
   };
 
   const disableElement = (text) => {
     return text?.trim();
   };
 
+  const setCurrentPage = (page) => {
+    setSearch((prev) => ({
+      ...prev,
+      currentPage: page,
+    }));
+  };
+
   const handleSubmit = (event) => {
+    const controller = new AbortController();
     event.preventDefault();
-    const trimmedTerm = term.trim();
-    trimmedTerm && onSearch(trimmedTerm);
+    const trimmedTerm = search.term.trim();
+    trimmedTerm && onSearch(trimmedTerm, controller);
   };
 
-  const onSearch = async (keyword) => {
-    if (!keyword.trim()) {
-      return {
-        data: [],
-        length: 0,
-      };
-    }
-    setIsLoading(true);
-    try {
-      const items = await AxiosInstance({
-        method: "GET",
-        url: `/media/search`,
-        params: {
-          keyword,
-          currentPage,
-        },
-      });
-
-      return items;
-    } catch (e) {
-      console.log(e);
-    } finally {
-      setIsLoading(false);
-    }
+  const refetchData = () => {
+    setSearch((prev) => ({
+      ...prev,
+      refetch: !prev.refetch,
+    }));
   };
+
+  const onSearch = useCallback(
+    async (keyword, controller) => {
+      if (!keyword?.trim()) {
+        return {
+          data: [],
+          length: 0,
+        };
+      }
+
+      setSearch((prev) => ({
+        ...prev,
+        isLoading: true,
+      }));
+
+      try {
+        const { data } = await AxiosInstance({
+          method: "GET",
+          url: `/media/search`,
+          params: {
+            keyword,
+            currentPage: search.currentPage,
+          },
+          signal: controller.signal,
+        });
+
+        setSearch((prev) => ({
+          ...prev,
+          searchItems: data.res,
+          total: data.length,
+          isLoading: false,
+        }));
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setSearch((prev) => ({
+          ...prev,
+          isLoading: false,
+        }));
+      }
+    },
+    [search.term, search.currentPage, search.submitClicked]
+  );
 
   useEffect(() => {
-    let timeout;
-    // setIsLoading(true);
-
-    timeout = setTimeout(async () => {
+    const controller = new AbortController();
+    try {
       const fetchItems = async () => {
-        const { data } = await onSearch(term);
-        setSearchItems(data.res);
-        setTotal(data.length);
+        await onSearch(search.term, controller);
       };
       fetchItems();
-    }, 1000);
-
-    const fetchItems = async () => {
-      const { data } = await onSearch(term);
-      setSearchItems(data.res);
-      setTotal(data.length);
-    };
-    fetchItems();
-
-    // setIsLoading(false);
-
+    } catch (e) {
+      console.log(e);
+    }
     return () => {
-      clearTimeout(timeout);
+      controller.abort();
     };
-  }, [submitClicked, currentPage]);
+  }, [search.currentPage, search.refetch]);
 
   return {
-    total,
-    isLoading,
-    searchItems,
+    total: search.total,
+    isLoading: search.isLoading,
+    searchItems: search.searchItems,
     handleSubmit,
-    term,
+    term: search.term,
     setTerm,
     disableElement,
     handleChange,
-    setSubmitClicked,
+    handleSubmitClicked,
     setCurrentPage,
-    currentPage,
+    currentPage: search.currentPage,
+    refetchData,
   };
 };
 
